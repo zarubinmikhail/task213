7f52792719a766d2507e050aa869b29cb78935fa
fradkinshtein <78609486+fradkinshtein@users.noreply.github.com>
2021-05-25 23:08:27 +0300

    rev0

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -10,16 +10,5 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
-    @Bean(name="dog")
-    public Dog getDog(){
-        return new Dog();
-    }
-    @Bean(name="cat")
-    public Cat getCat(){
-        return new Cat();
-    }
-    @Bean(name="timer")
-    public Timer getTimer(){
-        return new Timer();
-    }
+
 }
